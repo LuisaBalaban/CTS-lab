1615078aab58d224cfa838a91a95108f9100a5b2
LuisaBalaban <67561596+LuisaBalaban@users.noreply.github.com>
2021-03-09 11:33:07 +0200

    final

---
+++ sem3CTS/src/g1092/refactor/phase2/ProductType.java
@@ -0,0 +1,13 @@
+package g1092.refactor.phase2;
+
+public enum ProductType {
+	NEW(0), DISCOUNTED(0.1f), LIMITED_STOCK(0.25f), LEGACY(0.35f);
+	private float discount;
+	private ProductType(float discountValue)
+	{
+		this.discount=discountValue;
+	}
+	public float getDisocunt() {
+		return this.discount;
+	}
+}
--- sem3CTS/src/g1092/refactor/phase2/SalesStrategy.java
+++ sem3CTS/src/g1092/refactor/phase2/SalesStrategy.java
@@ -1,29 +1,36 @@
 package g1092.refactor.phase2;
 
+import g1092.refactor.exceptions.InvalidValueException;
+
 public class SalesStrategy {
 	public static final int FIDELITY_YEARS_THRESHOLD=10;
 	public static final float MAX_FIDELITY_DISCOUNT=0.15f;
 	public float ComputeFinalPrize(
-			int productType, float initialPrice, int yearsSincerRegistration)
+			ProductType productType, float initialPrice, int yearsSinceRegistration) throws InvalidValueException
 	  {
-	    float rezultat = 0;
-	    float fidelityDiscount = (yearsSincerRegistration > FIDELITY_YEARS_THRESHOLD) ? MAX_FIDELITY_DISCOUNT : (float)yearsSincerRegistration/100; 
-	    if (productType == 1)
-	    {
-	      rezultat = initialPrice;
+		if(initialPrice<=0 || yearsSinceRegistration<0)
+		{
+			throw new InvalidValueException();
+		}
+		
+	    float finalPrice = 0;
+	    float fidelityDiscount = (yearsSinceRegistration > FIDELITY_YEARS_THRESHOLD) ? MAX_FIDELITY_DISCOUNT : (float)yearsSinceRegistration/100; 
+	    switch(productType) {
+	    case NEW:
+	    	finalPrice = initialPrice;
+	    	break;
+	    case DISCOUNTED:
+		      finalPrice = (initialPrice - (ProductType.DISCOUNTED.getDisocunt() * initialPrice)) - fidelityDiscount * (initialPrice - (ProductType.DISCOUNTED.getDisocunt() * initialPrice));
+		      break;
+	    case LIMITED_STOCK:
+		      finalPrice = (initialPrice - (ProductType.LIMITED_STOCK.getDisocunt() * initialPrice)) - fidelityDiscount * (initialPrice - (ProductType.LIMITED_STOCK.getDisocunt() * initialPrice));
+		      break;
+	    case LEGACY:
+		      finalPrice = (initialPrice - (ProductType.LEGACY.getDisocunt() * initialPrice)) - fidelityDiscount * (initialPrice - (ProductType.LEGACY.getDisocunt() * initialPrice));
+		      break;
+		default:
+			throw new UnsupportedOperationException("Type not managed");    
 	    }
-	    else if (productType == 2)
-	    {
-	      rezultat = (initialPrice - (0.1f * initialPrice)) - fidelityDiscount * (initialPrice - (0.1f * initialPrice));
-	    }
-	    else if (productType == 3)
-	    {
-	      rezultat = (initialPrice - (0.25f * initialPrice)) - fidelityDiscount * (initialPrice - (0.25f * initialPrice));
-	    }
-	    else if (productType == 4)
-	    {
-	      rezultat = (initialPrice - (0.35f * initialPrice)) - fidelityDiscount * (initialPrice - (0.35f * initialPrice));
-	    }
-	    return rezultat;
+	    return finalPrice;
 	  }
 }
